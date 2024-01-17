3764228698959d0ac4c8921c1e38e9f803be6597
ThomasStosskopf <thomasstosskopf@gmail.com>
2024-01-17 09:37:04 +0100

    change file names

---
+++ 01_benchmark/main_benchmark.py
@@ -0,0 +1,27 @@
+
+import sys
+
+# caution: path[0] is reserved for script path (or '' in REPL)
+sys.path.insert(1, '00-script/01-prepare_data/prepare_file.py')
+
+import prepare_file
+from 00-script.01-prepare_data import prepare_file
+from 00-script.01-prepare_data import prepare_graph
+
+
+
+class Main:
+    
+
+    def __init__(self) -> None:
+        pass
+
+    
+    def main(self)->None:
+
+        return None
+
+
+
+if __name__ == "__maim__":
+    print("Starting now .....")
