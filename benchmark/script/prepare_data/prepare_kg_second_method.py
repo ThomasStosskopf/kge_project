bba6e3a29da0c39ce87fc72a394a6fd067416758
ThomasStosskopf <thomasstosskopf@gmail.com>
2024-02-01 10:27:26 +0100

    add the method where we split data into train and test for each relation

--- benchmark/script/prepare_data/prepare_kg_second_method.py
+++ benchmark/script/prepare_data/prepare_kg_second_method.py
@@ -1,36 +1,110 @@
-from pandas import concat, DataFrame, merge
+from pandas import DataFrame, merge
 from sklearn.model_selection import train_test_split
 from prepare_kg import PrepareKG
 
 
 class PrepareKGSecondMethod(PrepareKG):
+    """
+    A class for preparing knowledge graphs using the second method.
+
+    This class extends the functionality of the PrepareKG class and provides methods
+    to process knowledge graphs based on specific rules and criteria. THe methods are used to remove
+    reverse relationship and redundant relationships.
+
+    Attributes:
+    - kg_path (str): The path to the knowledge graph file.
+    - output_nodes_map (str): The path to the output file containing node mappings.
+    - output_kg_edge_list (str): The path to the output file containing the knowledge graph edge list.
+    - output_train (str): The path to save the training set.
+    - output_test (str): The path to save the testing set.
+    - output_val (str): The path to save the validation set.
+
+    Methods:
+    - __init__(self, kg_path, output_nodes_map, output_kg_edge_list, output_train, output_test, output_val):
+        Initialize the PrepareKGSecondMethod object.
+
+    - find_reverse_to_remove(self, graph):
+        Find reverse relations to be removed from the graph.
+
+    - remove_reverse_relation(self, graph):
+        Remove reverse relations from the graph.
+
+    - find_redundant_relation(self, graph):
+        Find redundant relations in the graph.
+
+    - remove_redundant_relation(self, graph):
+        Remove redundant relations from the graph.
+
+    - split_train_test_val(self, graph, test_size, val_size, random_state):
+        Split the graph into training, testing, and validation sets.
+
+    - main(self):
+        Perform the main processing steps including graph expansion, relation removal,
+        splitting into train-test-validation sets, and saving the processed sets.
+    """
 
     def __init__(self, kg_path: str, output_nodes_map: str, output_kg_edge_list: str,
                  output_train="benchmark/data/train_set_second_method.csv",
                  output_test="benchmark/data/test_set_second_method.csv",
                  output_val="benchmark/data/val_set_second_method.csv"):
+        """
+        Initialize the PrepareKGSecondMethod object.
+
+        Parameters:
+        - kg_path (str): The path to the knowledge graph file.
+        - output_nodes_map (str): The path to the output file containing node mappings.
+        - output_kg_edge_list (str): The path to the output file containing the knowledge graph edge list.
+        - output_train (str, optional): The path to save the training set. Defaults to "benchmark/data/train_set_second_method.csv".
+        - output_test (str, optional): The path to save the testing set. Defaults to "benchmark/data/test_set_second_method.csv".
+        - output_val (str, optional): The path to save the validation set. Defaults to "benchmark/data/val_set_second_method.csv".
+        """
         super().__init__(kg_path, output_nodes_map, output_kg_edge_list, output_train, output_test, output_val)
 
     def find_reverse_to_remove(self, graph: DataFrame) -> DataFrame:
+        """
+        Find reverse relations to be removed from the graph.
+
+        Parameters:
+        - graph (DataFrame): The DataFrame representing the input graph.
+
+        Returns:
+        - DataFrame: DataFrame containing reverse relations to be removed.
+        """
         merged_df = merge(graph, graph, left_on=['from', 'to'], right_on=['to', 'from'], suffixes=('_first', '_reverse'))
         reverse_to_remove = merged_df[['from_reverse', 'to_reverse', 'rel_reverse']].copy()
         reverse_to_remove.columns = ['from','to','rel']
-        print(reverse_to_remove)
         return reverse_to_remove
 
     def remove_reverse_relation(self, graph: DataFrame) -> DataFrame:
+        """
+        Remove reverse relations from the graph.
+
+        Parameters:
+        - graph (DataFrame): The DataFrame representing the input graph.
+
+        Returns:
+        - DataFrame: DataFrame representing the graph with reverse relations removed.
+        """
         reverse_to_remove = self.find_reverse_to_remove(graph)
         graph_wo_reverse = graph[
             ~graph.set_index(['from', 'to']).index.isin(reverse_to_remove.set_index(['from', 'to']).index)]
-        graph_wo_reverse.to_csv('benchmark/data/graph_wo_reverse.csv', sep="\t", index=False)
         return graph_wo_reverse
 
     def find_redundant_relation(self, graph):
-        # Grouper les données par les colonnes "from" et "to" et compter le nombre de valeurs uniques pour la colonne "rel"
+        """
+        Find redundant relations in the graph.
+
+        Parameters:
+        - graph (DataFrame): The DataFrame representing the input graph.
+
+        Returns:
+        - DataFrame: DataFrame containing redundant relations in the graph.
+        """
+        # Group the data by the "from" and "to" columns and count the number of unique values in the "rel" column.
         grouped = graph.groupby(['from', 'to'])['rel'].nunique()
-        # Filtrer les groupes où le nombre de valeurs uniques pour "rel" est supérieur à 1 (relations redondantes)
+        # Filter groups where the number of unique values for "rel" is greater than 1 (redundant relationships)
         redundant_groups = grouped[grouped > 1].reset_index()
-        # Fusionner les données d'origine avec les groupes redondants pour obtenir les relations redondantes complètes
+        # Merge original data with redundant groups to obtain complete redundant relationships
         redundant_relations = merge(graph, redundant_groups, on=['from', 'to'], how='inner')
         reduced_relation = redundant_relations.drop(redundant_relations.index[::2]).reset_index(drop=True)
         redundant_relations = reduced_relation[['from', 'to', 'rel_x']].copy()
@@ -39,11 +113,19 @@ class PrepareKGSecondMethod(PrepareKG):
 
 
     def remove_redundant_relation(self, graph: DataFrame) -> DataFrame:
+        """
+        Remove redundant relations from the graph.
+
+        Parameters:
+        - graph (DataFrame): The DataFrame representing the input graph.
+
+        Returns:
+        - DataFrame: DataFrame representing the graph with redundant relations removed.
+        """
         redundant_to_remove = self.find_redundant_relation(graph)
         graph_wo_redundant = graph[
             ~graph.set_index(['from', 'to', 'rel']).index.isin(redundant_to_remove.set_index(['from', 'to', 'rel']).index)]
-        print(graph_wo_redundant)
-        graph_wo_redundant.to_csv('benchmark/data/graph_wo_redundant.csv', sep="\t", index=False)
+
         return graph_wo_redundant
 
     def split_train_test_val(self, graph: DataFrame, test_size=0.2, val_size=0.1, random_state=None) -> (
@@ -77,14 +159,19 @@ class PrepareKGSecondMethod(PrepareKG):
         return train_set, test_set, val_size
 
     def main(self):
+        """
+        Perform the main processing steps including graph expansion, relation removal,
+        splitting into train-test-validation sets, and saving the processed sets.
+        """
         full_graph, new_nodes = self.generate_edgelist()
 
         full_graph = self.expand_graph_relations(full_graph)
         print(f"FULL_GRAPH BEFORE SAVING:\n{full_graph}")
         self.saving_dataframe(full_graph, new_nodes)
-        self.print_relations_count(full_graph)
         full_graph = self.remove_reverse_relation(full_graph)
         full_graph = self.remove_redundant_relation(full_graph)
+        self.print_relations_count(full_graph)
+        print(full_graph)
         train, test, val = self.split_train_test_val(full_graph, random_state=3)
         self.save_train_test_val(train=train, test=test, val=val)
 
---
+++ benchmark/script/prepare_data/prepare_kg_third_method.py
@@ -0,0 +1,86 @@
+from pandas import DataFrame, merge, concat
+from sklearn.model_selection import train_test_split
+from prepare_kg_second_method import PrepareKGSecondMethod
+
+
+class PrepareKGThirdMethod(PrepareKGSecondMethod):
+
+    def __init__(self, kg_path: str, output_nodes_map: str, output_kg_edge_list: str,
+                 output_train="benchmark/data/train_set_third_method.csv",
+                 output_test="benchmark/data/test_set_third_method.csv",
+                 output_val="benchmark/data/val_set_third_method.csv"):
+        super().__init__(kg_path, output_nodes_map, output_kg_edge_list, output_train, output_test, output_val)
+
+    def get_unique_values(self, graph: DataFrame, column_name: str) -> list:
+        return graph[column_name].unique()
+
+    def split_dataframe_based_on_relation(self, graph: DataFrame, column_name: str, unique_rel: list) -> dict:
+        relation_dataframes = {}
+        for rel in unique_rel:
+            rel_df = graph[graph[column_name] == rel].copy()
+            relation_dataframes[rel] = rel_df
+        return relation_dataframes
+
+    def split_each_dataframe_into_train_test_val(self, relations_dict_dataframe: dict, test_size=0.2,
+                                                 val_size=0.1, random_state=None) -> dict:
+        relation_train_test_val_splits = {}
+        for rel, rel_df in relations_dict_dataframe.items():
+            train_set, test_set = train_test_split(rel_df, test_size=test_size, random_state=random_state)
+            train_set, val_set = train_test_split(train_set, test_size=val_size, random_state=random_state)
+            relation_train_test_val_splits[rel] = (train_set, test_set, val_set)
+        return relation_train_test_val_splits
+
+    def concat_split_sets(self, relation_train_test_val_splits:dict) -> tuple:
+        """
+        Concatenate the split sets for each relation type into one big dataframe per set type.
+
+        Parameters:
+        - relation_train_test_val_splits (dict): A dictionary where each key is a relation type, and the corresponding
+                                                 value is a tuple containing DataFrames representing the training,
+                                                 testing, and validation sets, respectively.
+
+        Returns:
+        - tuple[DataFrame, DataFrame, DataFrame]: A tuple containing DataFrames representing the concatenated
+                                                   training, testing, and validation sets, respectively.
+        """
+        # Initialize empty DataFrames for concatenated sets
+        concatenated_train_set = DataFrame()
+        concatenated_test_set = DataFrame()
+        concatenated_val_set = DataFrame()
+
+        # Concatenate sets for each relation type
+        for rel, (train_set, test_set, val_set) in relation_train_test_val_splits.items():
+            concatenated_train_set = concat([concatenated_train_set, train_set])
+            concatenated_test_set = concat([concatenated_test_set, test_set])
+            concatenated_val_set = concat([concatenated_val_set, val_set])
+
+        return concatenated_train_set, concatenated_test_set, concatenated_val_set
+
+    def main(self) -> None:
+        """
+        Perform the main processing steps including graph expansion, relation removal,
+        splitting into train-test-validation sets, and saving the processed sets.
+        """
+        full_graph, new_nodes = self.generate_edgelist()
+
+        full_graph = self.expand_graph_relations(full_graph)
+        print(f"FULL_GRAPH BEFORE SAVING:\n{full_graph}")
+        self.saving_dataframe(full_graph, new_nodes)
+        self.print_relations_count(full_graph)
+        full_graph = self.remove_reverse_relation(full_graph)
+        full_graph = self.remove_redundant_relation(full_graph)
+        unique_rel = self.get_unique_values(graph=full_graph, column_name="rel")
+        rel_df = self.split_dataframe_based_on_relation(graph=full_graph, column_name="rel", unique_rel=unique_rel)
+        dict_train_test_split = self.split_each_dataframe_into_train_test_val(relations_dict_dataframe=rel_df,
+                                                                              random_state=3)
+
+        train_set, test_set, val_set = self.concat_split_sets(relation_train_test_val_splits=dict_train_test_split)
+        self.save_train_test_val(train=train_set, test=test_set, val=val_set)
+   
+
+if __name__ == "__main__":
+    prepare_kg = PrepareKGThirdMethod(kg_path='benchmark/data/kg_giant_orphanet.csv',
+                           output_nodes_map="benchmark/data/KG_node_map_THIRD_METHOD.txt",
+                           output_kg_edge_list="benchmark/data/KG_edgelist_mask_THIRD_METHOD.txt")
+
+    prepare_kg.main()
